87db92431c6aecc70cd68edaa937bcbb9159230c
hukai <huzhengen@163.com>
2021-04-25 10:21:44 +0800

    Create User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
-class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::API
+
+  def render_resource(resource)
+    return head 404 if resource.nil?
+    if resource.errors.empty?
+      render json: { resource: resource }, status: 200
+    else
+      render json: { errors: resource.errors }, status: 422
+    end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,18 +20,21 @@ class UsersController < ApplicationController
   end
 
   # POST /users or /users.json
+  # def create
+  #   @user = User.new(user_params)
+  #
+  #   respond_to do |format|
+  #     if @user.save
+  #       format.html { redirect_to @user, notice: "User was successfully created." }
+  #       format.json { render :show, status: :created, location: @user }
+  #     else
+  #       format.html { render :new, status: :unprocessable_entity }
+  #       format.json { render json: @user.errors, status: :unprocessable_entity }
+  #     end
+  #   end
+  # end
   def create
-    @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: "User was successfully created." }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    render_resource User.create create_params
   end
 
   # PATCH/PUT /users/1 or /users/1.json
@@ -57,6 +60,10 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def create_params
+    params.permit(:email, :password, :password_confirmation)
+  end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_user
